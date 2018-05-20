efdf0747947aea8921b869b0f512827f46d1d9fd
Sindhu Shankar <sindhu.pathfinder@gmail.com>
2018-05-20 12:02:00 -0700

    implemented url_for - to edit and delete restaurants

--- servers-using templates/restaurantsMenu.py
+++ servers-using templates/restaurantsMenu.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -10,14 +10,24 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+@app.route('/restaurants/<int:restaurant_id>/edit')
+def editRestaurant(restaurant_id):
+    return 'edit url added'
+
+
+@app.route('/restaurants/<int:restaurant_id>/delete')
+def deleteRestaurant(restaurant_id):
+    return 'delete url added'
+
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
     print"helllooooo"
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    return render_template('menu.html',restaurant = restaurant, items = items)
-    
+    editUrl = url_for('editRestaurant',restaurant_id=restaurant_id)
+    deleteUrl= url_for('deleteRestaurant',restaurant_id = restaurant_id)
+    return render_template('menu.html',restaurant = restaurant, items = items, editURL = editUrl, deleteUrl = deleteUrl)    
     
 if __name__ == '__main__':
     app.debug = True
