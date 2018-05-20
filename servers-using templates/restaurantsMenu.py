11d20a7227a434772664f6e048f8ae0aef2560de
Sindhu Shankar <sindhu.pathfinder@gmail.com>
2018-05-20 13:05:39 -0700

    implemented url_for within templates - dynamically fetch the path

--- servers-using templates/restaurantsMenu.py
+++ servers-using templates/restaurantsMenu.py
@@ -10,24 +10,20 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-@app.route('/restaurants/<int:restaurant_id>/edit')
-def editRestaurant(restaurant_id):
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
     return 'edit url added'
 
-
-@app.route('/restaurants/<int:restaurant_id>/delete')
-def deleteRestaurant(restaurant_id):
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
     return 'delete url added'
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
-    print"helllooooo"
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    editUrl = url_for('editRestaurant',restaurant_id=restaurant_id)
-    deleteUrl= url_for('deleteRestaurant',restaurant_id = restaurant_id)
-    return render_template('menu.html',restaurant = restaurant, items = items, editURL = editUrl, deleteUrl = deleteUrl)    
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)    
+    return render_template('menu.html',restaurant = restaurant, items = items)    
     
 if __name__ == '__main__':
     app.debug = True
