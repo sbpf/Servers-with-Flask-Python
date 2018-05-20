355beff634b396b08845519c522e25c5719afd4c
Sindhu Shankar <sindhu.pathfinder@gmail.com>
2018-05-20 11:14:39 -0700

    restaurant server using "render templates"

---
+++ servers-using templates/restaurantsMenu.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+app = Flask(__name__)
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+@app.route('/')
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    print"helllooooo"
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
+    return render_template('menu.html',restaurant = restaurant, items = items)
+    
+    
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0',port = 5000)
