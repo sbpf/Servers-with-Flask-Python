d4c53e8fcf067ba6c620d57766747da98e676a42
Sindhu Shankar <sindhu.pathfinder@gmail.com>
2018-05-23 16:40:06 -0700

    working prototype

--- servers-using templates/restaurantsMenu.py
+++ servers-using templates/restaurantsMenu.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -19,6 +19,7 @@ def addNewMenuItem(restaurant_id):
                                restaurant_id = restaurant_id)
         session.add(newMenuItem)
         session.commit()
+        flash("New menu item created!")
         return redirect(url_for('restaurantMenu',restaurant_id = restaurant_id))
     else:
         return render_template('addMenuItem.html',restaurant_id = restaurant_id)
@@ -32,6 +33,7 @@ def editMenuItem(restaurant_id, menu_id):
         menuItem.description = request.form['description']
         session.add(menuItem)
         session.commit()
+        flash("Menu item updated successfully!")
         return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
     
     else:
@@ -43,19 +45,27 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':        
             item = session.query(MenuItem).filter_by(id = menu_id).one()
             session.delete(item)
-            session.commit()        
+            session.commit()
+            flash("Menu item deleted!")
             return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
     else:
         item = session.query(MenuItem).filter_by(id = menu_id).one()
         return render_template('deleteMenuItem.html', menuItem = item)
 
-@app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)    
-    return render_template('menu.html',restaurant = restaurant, items = items)    
+    return render_template('menu.html',restaurant = restaurant, items = items)
+
+@app.route('/')
+@app.route('/restaurants/')
+def restaurants():
+    allRestaurants = session.query(Restaurant).all()
+    return render_template('restaurants.html', restaurants = allRestaurants)
     
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host = '0.0.0.0',port = 5000)
+    app.secret_key = 'super_secret_key'
+    #app.debug = True
+    #app.run(host = '0.0.0.0',port = 5000)
+    app.run()
